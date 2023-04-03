36a29341f4e72eff2a07ba55722d48a2f02b0cac
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-04-03 19:34:36 +0500

    set app.js with basics express add-ons

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>My SQL app</h1>');
+});
+
+app.listen(3000, () => {
+    console.log('The app is running on localhost:3000');
+});
