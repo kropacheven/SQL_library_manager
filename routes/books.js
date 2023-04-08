10e63d0c6a8172c40ff9c73f860017a68a6535b5
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-04-08 21:26:44 +0500

    Add Book model to books route
    Plus add routes handler function

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource - new books model');
-});
+//Importing Book model from models folder:
+const Book = require('../models').Book;
 
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
+
+/* GET books listing. */
+router.get('/', asyncHandler(async (req, res) => {
+  //res.render("articles/index", { articles: {}, title: "Sequelize-It!" });
+  res.send('Hello from Books!!!')
+}));
+
+// Module exports:
 module.exports = router;
