d2b80e05c4e31dfddbb809e9a4dba2e98dcf0cb9
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-04-08 22:05:43 +0500

    Create one central route - index.js
    Moving data from books.js route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.redirect('/books' );
-});
+//Importing Book model from models folder:
+const Book = require('../models').Book;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
+
+/* GET books listing. */
+router.get('/', asyncHandler(async (req, res) => {
+  //res.render("articles/index", { articles: {}, title: "Sequelize-It!" });
+  res.render('index')
+}));
+
 
 module.exports = router;
