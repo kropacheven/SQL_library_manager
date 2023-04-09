1f94e10a83ef7af4f9454e43f8a7bbf4ccf2819c
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-04-09 15:03:55 +0500

    Creating book.js route in routes
    All logic is transferred in this route from index.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 
 //Require routes
 const indexRouter = require('./routes');
-//const booksRouter = require('./routes/books');
+const booksRouter = require('./routes/books');
 
 
 // Run the app with express:
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Middleware for above required routes:
 app.use('/', indexRouter);
-//app.use('/books', booksRouter);
+app.use('/books', booksRouter);
 
 
 // catch 404 and forward to error handler
---
+++ routes/books.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+//Importing Book model from models folder:
+const Book = require('../models').Book;
+
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+    return async(req, res, next) => {
+      try {
+        await cb(req, res, next)
+      } catch(error){
+        // Forward error to the global error handler
+        next(error);
+      }
+    }
+  }
+  
+  /* GET books listing. */
+router.get('/', asyncHandler(async (req, res) => {
+    const books = await Book.findAll();
+    res.render("index", { books: books });
+    //res.render('index')
+  }));
+  
+  
+  module.exports = router;
